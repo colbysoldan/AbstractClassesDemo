ae83a813fa6108c2d12ba238d4fbff939ec45e25
Colby Soldan <colbylsoldan@gmail.com>
2022-06-16 07:25:01 -0500

    vehicle abstract class

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public abstract class Vehicle
+	{
+		public string Year { get; set; } = "year"; //default values
+		public string Make { get; set; } = "make";
+		public string Model { get; set; } = "model";
+
+		public abstract void DriveAbstract(); //stubbed out method
+
+		public virtual void DriveVirtual() //base implementation
+        {
+			Console.WriteLine("Drive");
+        }
+
+		public Vehicle()
+		{
+		}
+	}
+}
+
